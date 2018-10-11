b09369220f04ffda1803a86f8c1b30896b089103
Gerard98 <35579458+Gerard98@users.noreply.github.com>
2018-10-11 18:57:06 +0200

    Update bubble_sort.java

--- bubble_sort/java/bubble_sort.java
+++ bubble_sort/java/bubble_sort.java
@@ -1,1 +1,15 @@
 // add your code here :
+
+public static int[] bubblesort(int tab[]){
+
+        int n = tab.length;
+        int tmp = 0;
+        for(int i=0;i<n;i++){
+            for(int j=1;j<n-1;j++){
+                tmp = tab[j-1];
+                tab[j-1] = tab[j];
+                tab[j] = tmp;
+            }
+        }
+
+    }
