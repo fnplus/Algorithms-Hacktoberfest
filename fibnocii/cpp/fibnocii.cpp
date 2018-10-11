905d36afb09779e0cd49883524d2f918d6e04439
ernestst <44066820+ernestst@users.noreply.github.com>
2018-10-11 19:47:26 +0200

    My version of calculating fibonacci number
    It is one of the best algorithm to calculate fibonacci number. It is simple and very fast code.

--- fibnocii/cpp/fibnocii.cpp
+++ fibnocii/cpp/fibnocii.cpp
@@ -1,2 +1,17 @@
-// add your code here
-
+/*
+@param a represents first of fibonacci number
+@param b represents second of fibonacci number
+@param c represents third of fibonacci number
+@param n represents which fibonacci number we would like to get
+*/
+int Fibo( int n ){
+ int a = 0;
+ int b = 1;
+ int c = 1;
+  for(int i =0 ; i< n; i++){
+    a = b;
+    b = c;
+    c = a + b;
+  }
+  return c;
+}
