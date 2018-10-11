999da6a9de4645063adb3ae4f6afdcc8c18d5525
jraffe2 <32085971+jraffe2@users.noreply.github.com>
2018-10-11 15:34:00 -0500

    Start of a binary search algorithm

--- binary_search/python/binary_search.py
+++ binary_search/python/binary_search.py
@@ -1,1 +1,28 @@
 # add your code here
+def binary_search(ordered_list, target_val):
+    low = 0
+    high = len(ordered_list)
+
+    while low <= high:
+
+        pivot = (len(ordered_list))/2
+        if ordered_list[pivot] == target_val:
+            return pivot
+        if target_val < ordered_list[pivot]:
+            high = pivot - 1
+
+        if target_val > ordered_list[pivot]:
+            low = pivot + 1
+
+        else:
+            print ("value not in our list")
+            return -1
+
+    arr = [7, 10, 16, 28, 34, 40]
+    search_val = 7
+    result = binary_search(arr, search_val)
+
+    if result == -1:
+        print "The value your looking for is not here"
+    else:
+        print "The search value is at element %d"
