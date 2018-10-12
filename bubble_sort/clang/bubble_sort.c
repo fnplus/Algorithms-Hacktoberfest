49c595e9e9c0632d728cc4fcc099430ade3a7702
Thomas Chadwick <thomas.chadwick2@gmail.com>
2018-10-11 20:39:29 -0600

    Add obvious test case for c bubble sort.

--- bubble_sort/clang/bubble_sort.c
+++ bubble_sort/clang/bubble_sort.c
@@ -37,7 +37,7 @@ void bubbleSort(int arr[], int size)
 
 int main()
 {
-    int arr[] = {64, 34, 25, 12, 22, 11, 90}; 
+    int arr[] = {7, 6, 5, 4, 3, 2, 1};  
     int n = sizeof(arr)/sizeof(arr[0]); 
     bubbleSort(arr, n); 
     printf("Sorted array: \n"); 
