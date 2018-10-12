93a07eef36d6ebfc0231ab09a48627938d974068
Thomas Chadwick <thomas.chadwick2@gmail.com>
2018-10-11 20:34:25 -0600

    Add bubble sort algorithm for c
    Iterates through an array and compares the current element to the next element. If the current element is larger than the next, the elements are swapped. If an iteration through the array does not have a swap, the loop is stopped.
    
    Closes https://github.com/fnplus/Algorithms-Hacktoberfest/issues/11

--- bubble_sort/clang/bubble_sort.c
+++ bubble_sort/clang/bubble_sort.c
@@ -1,1 +1,46 @@
-//add your code here
+#include <stdio.h>
+#include <stdbool.h>
+
+void print(int arr[], int size) 
+{ 
+    int i; 
+    for (i=0; i < size; i++) 
+        printf("%d ", arr[i]);
+}
+
+void swap(int *x, int *y) 
+{ 
+    int temp = *x; 
+    *x = *y; 
+    *y = temp; 
+} 
+
+void bubbleSort(int arr[], int size) 
+{
+    bool haveSwapped;
+    int i, j = 0;
+    
+    do
+    {
+        haveSwapped = false;
+        for (i = 0; i < size - j - 1; i++)
+        {
+            if (arr[i] > arr[i+1])
+            {
+                swap(&arr[i], &arr[i+1]); 
+                haveSwapped = true; 
+            }
+        }
+        j++;
+    } while (haveSwapped);
+}
+
+int main()
+{
+    int arr[] = {64, 34, 25, 12, 22, 11, 90}; 
+    int n = sizeof(arr)/sizeof(arr[0]); 
+    bubbleSort(arr, n); 
+    printf("Sorted array: \n"); 
+    print(arr, n); 
+    return 0; 
+}
