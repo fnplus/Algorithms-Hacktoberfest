b464a39c3ab6caaf476f9b2b6d2fd8e91312586b
Arjun Mahishi <arjun.mahishi@gmail.com>
2018-10-14 00:43:25 +0530

    Added python snippet for binary search

--- binary_search/python/binary_search.py
+++ binary_search/python/binary_search.py
@@ -1,1 +1,12 @@
-# add your code here
+# Assuming the array is sorted in ascending order
+def binarySearch(array, n):
+    size = len(array)/2
+    if len(array) == 0:
+        print("Element not found")
+    elif array[size] == n:
+        print ("Element found")
+    elif array[size] > n:
+        binarySearch(array[:size], n)
+    else:
+        binarySearch(array[size+1:], n)
+        
