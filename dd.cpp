d0a8c046fd4bdc947abd5b8fe8a55656a742d8f8
Arjunann <44053189+Arjunann@users.noreply.github.com>
2018-10-13 18:06:03 +0530

    the changes is done

--- dd.cpp
+++ dd.cpp
@@ -1,18 +1,57 @@
-#include <stdio.h>      
-#include <stdlib.h>     
-
-int values[] = { 40, 10, 100, 90, 20, 25 };
-
-int compare (const void * a, const void * b)
+#include<iostream>
+ 
+using namespace std;
+ 
+// A function implementing Counter sort.
+void CounterSort(int a[], int n, int r, int lower)
 {
-  return ( *(int*)a - *(int*)b );
+	int i, j = 0, counter[r] = {0};	
+	// Counting the number occurrence of each element.
+	for(i=0; i<n; i++)
+		counter[a[i]-lower]++;
+ 
+	i=0;
+	// placing the elements back into array.
+	while(i < r)
+	{
+		flag:
+		a[j] = lower+i;
+		j++;
+		counter[i]--;
+ 
+		// place the same element until its counter is zero.
+		if(counter[i] > 0)
+		goto flag;
+ 
+		i++;
+	}
 }
-
-int main ()
+ 
+int main()
 {
-  int n;
-  qsort (values, 6, sizeof(int), compare);
-  for (n=0; n<6; n++)
-     printf ("%d ",values[n]);
-  return 0;
+	int n, i, range, ulimit, llimit;
+	cout<<"\nEnter the number of data element to be sorted: ";
+	cin>>n;
+ 
+	cout<<"\nEnter the lower and upper limit of the data to be entered: ";
+	cin>>llimit>>ulimit;
+ 
+	// Range of the input data. 
+	range = ulimit-llimit+1;
+ 
+	int arr[n];
+	for(i = 0; i < n; i++)
+	{
+		cout<<"Enter element "<<i+1<<": ";
+		cin>>arr[i];
+	}
+ 
+	CounterSort(arr, n, range, llimit);
+ 
+	// Printing the sorted data.
+	cout<<"\nSorted Data ";
+	for (i = 0; i < n; i++)
+        	cout<<"->"<<arr[i];
+ 
+	return 0;
 }
