ad30f3f8ca3144ea86280fa6ed4b721cd99255f6
jraffe2 <32085971+jraffe2@users.noreply.github.com>
2018-10-13 19:34:34 -0500

    Now allows a user to input array values and choose a search value

--- binary_search/python/binary_search.py
+++ binary_search/python/binary_search.py
@@ -13,15 +13,29 @@ def binary_search(ordered_list,low,high,target_value):
         elif target_value < ordered_list[midpoint]:
             high = midpoint-1
         
-    print ("Your search value is not in our list. Please try again.")
+    print ("\nYour search value is not in our list.")
     return -1
     
-arr = [7, 10, 16, 28, 34, 40]
+while (True):
+    arr = []
+    n = int(raw_input('Enter how many elements you want: '))
+    print 'Enter your inputs '
+    
+    for i in range(0, n):
+        arr.append(input())
+    print(arr)
 
-search_val = 17
-low = 0
-high = len(arr)-1
+    search_val =  int(raw_input('Enter a number to search for in the list '))
+    low = 0
+    high = len(arr)-1
 
-result = binary_search(arr,low,high,search_val)
-if result != -1:
-    print "Success! The search value is at element " + str(result)
+    result = binary_search(arr,low,high,search_val)
+    if result != -1:
+        print "\nSuccess! The search value is at element " + str(result)
+
+    terminate = (raw_input('\nEnter y to search again, or x to exit. '))
+
+    if terminate == 'x':
+        break
+    if terminate == 'y':
+        continue
