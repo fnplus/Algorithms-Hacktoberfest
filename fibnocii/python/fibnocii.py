76b336db11592c67463369e937fa29e6a8db21b3
Souma Kanti Ghosh <40127554+SubCoder1@users.noreply.github.com>
2018-10-15 23:17:51 +0530

    Fibonacci Sequence implementation in Python3
    >Directly enter the range upto which your fibonacci sequence is gonna be shown.
    > Get the sequence as an output in a newline.

--- fibnocii/python/fibnocii.py
+++ fibnocii/python/fibnocii.py
@@ -1,1 +1,11 @@
-# add your code here
+def fib():
+    a, b = 0, 1
+    while True:
+        yield a            
+        a, b = b, a + b
+
+print("Enter Range -- ",end='')
+Range = int(input())
+print("Generated Fibonacci Sequence -- ")
+for i,j in zip(range(Range), fib()):
+    print(j,end=' ')
