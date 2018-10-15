daaca37d6acf0e1f39f75a26021edc9bf200133f
Ilya <ilia@alov.me>
2018-10-15 21:50:15 +0300

    Update euclidean_gcd.java

--- euclidean_gcd/java/euclidean_gcd.java
+++ euclidean_gcd/java/euclidean_gcd.java
@@ -1,1 +1,10 @@
-// add your code here :
+public class GCD {
+    public static int gcd(int a,int b) {
+        while (b !=0) {
+            int tmp = a%b;
+            a = b;
+            b = tmp;
+        }
+        return a;
+    }
+}
