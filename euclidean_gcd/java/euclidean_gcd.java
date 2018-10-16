c8f91f3aecb043911dd3e0e12aa974382b263aa3
Saurav Jaiswal <sauravjaiswal999@gmail.com>
2018-10-16 23:41:39 +0530

    Revert "adds euclidean gcd method (#51)"
    This reverts commit beebeace7008fedea41b5ee8b8dd53c68a00962e.

--- euclidean_gcd/java/euclidean_gcd.java
+++ euclidean_gcd/java/euclidean_gcd.java
@@ -1,14 +1,1 @@
-import java.util.Scanner; 
-  
-public class GCD { 
-    public static int gcd(int a, int b) { 
-        if (a == 0) return b; 
-        return gcd(b%a, a); 
-    }
-    public static void main(String[] args) { 
-        Scanner in=new Scanner(System.in);
-        int a = in.nextInt();
-        int b = in.nextInt(); 
-        System.out.println("GCD(" + a +  " , " + b+ ") = " + gcd(a,b)); 
-    } 
-} 
+// add your code here :
