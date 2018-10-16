317a2463814169fa651a252e8be1847f806f834d
Souma Kanti Ghosh <40127554+SubCoder1@users.noreply.github.com>
2018-10-16 17:57:33 +0530

    counting sort implementation in C++

--- counting_sort/cpp/update_counting_sort.cpp
+++ counting_sort/cpp/update_counting_sort.cpp
@@ -1,46 +1,49 @@
-/* Created by SubCoder1 on 31-May-18.
+#include <iostream>
+using namespace std;
 
-     Sorting Technique -- Counting Sort
-     Input             -- A sequence of n numbers and Range
-     Output            -- Reordered into a definite sequence {Ascending(HERE)}
-
-     Time Complexity :-
-        Worst Case - O(N^2)
-        Best  Case - O(N + RANGE)
- */
-
-#include <bits/stdc++.h>
-#include <chrono>
-
- using namespace std::chrono;
- using namespace std;
-
-int main(){
-    int inpt, range;
-    vector<int> store;
-     
-    cout << "Give the range of input -- ";
-    cin << range;
-
-    cout << "\nUnsorted Input -- ";
-    while(cin.peek() != '\n'){
-        cin >> inpt;
-        store.push_back(inpt);
+void countingSort(int arr[],int n,int RANGE){
+    int count[RANGE]={0};
+    int i;
+    int out[n];
+    
+    
+    for(i=0;i<n;i++)
+    ++count[arr[i]];
+    
+    for(i=1;i<RANGE;i++)
+    count[i]+=count[i-1];
+    
+   
+    
+    for(i=n-1;i>=0;i--){
+        out[count[arr[i]]-1]=arr[i];
+        --count[arr[i]];
     }
     
-    auto start = high_resolution_clock::now();
- 
-    vector<int> aux(range, 0);
+    for(i=0;i<n;i++)
+    arr[i]=out[i];
+    
+    
+    
+}
+void print(int arr[],int n){
+    cout<<"array : ";
+    for(int i=0;i<n;i++)
+    cout<<arr[i]<<' ';
+    cout<<endl;
+}
 
-    for(auto i: store){ aux[i-1]+=1; }
-
-    cout << "Sorted Output -- ";
-    for(int i = 0; i < range; i++){
-        for(int j = 1; j <= aux[i]; j++){
-            cout << i+1 << " ";
-        }
-    }
-
-    auto end = high_resolution_clock::now();
-    cout << "Time Elapsed -- " << duration_cast<microseconds>(end - start).count() << " microS.\n";
+int main() {
+	// your code goes here
+	int arr[]={1, 4, 1, 2, 7, 5, 2};
+	int n=sizeof(arr)/sizeof(arr[0]);
+	int RANGE=9;
+	
+	print(arr,n);
+	
+	countingSort(arr,n,RANGE);
+	
+    print(arr,n);
+	
+	return 0;
 }
