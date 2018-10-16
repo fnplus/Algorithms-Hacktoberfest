39fa090b7960494efbde3d947ada01c022daf4f3
Souma Kanti Ghosh <40127554+SubCoder1@users.noreply.github.com>
2018-10-16 18:04:19 +0530

    Binary search implementation in Python 3

--- binary_search/python/update_binary_search.py
+++ binary_search/python/update_binary_search.py
@@ -1,19 +1,18 @@
 def b_Search(array, value):
-    sTart = 0
-    eNd = len(array)
+    start = 0
+    end = len(array)
     found = False
     pos = None
-    while sTart <= eNd and not found :
-        mid = (sTart + eNd) // 2
+    while start <= end and not found :
+        mid = (start + end) // 2
         if value == array[mid] : found = True ; pos = mid
         elif value < array[mid] : end = mid - 1
         else : start = mid + 1
     if found : print("Element found at Position {position}".format(position=pos+1))
     else : print("Element not in the list")
 
-print("Enter the Sequence of elements -- ", end='')
-data = list(map(int, input().split()))
-data.sort()
-print("Enter the element to be searched in the data given -- ", end='')
-element = int(input())
-b_Search(data,element)
+array = [3,4,5,6,7,8,9,10]
+print("given list of numbers -- " + str(array))
+val_to_be_searched = 10
+print("Value to be searched -- " + str(val_to_be_searched))
+b_Search(array,val_to_be_searched)
