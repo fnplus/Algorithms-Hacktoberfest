8b7083d87562236533299345a6fd4799417e7ae2
Paul T <DeveloperPaul123@users.noreply.github.com>
2018-10-18 16:55:26 -0400

    Added Quick Sort Algorithm in C++ (#75)
    Thanks for the contribution issue has been solved.

--- quick_sort/cpp/quick_sort.cpp
+++ quick_sort/cpp/quick_sort.cpp
@@ -1,67 +1,79 @@
-/* Created by SubCoder1 on 31-May-18.
-     
-     Sorting Technique -- Quick Sort
-     Input             -- A sequence of n numbers
-     Output            -- Reordered into a definite sequence {Ascending(HERE)}
-
-     Time Complexity :- 
-        Worst Case - O(N^2)
-        Average Case - O(NlogN)
-        Best  Case - O(NLogN)
- */
-
-
 #include <iostream>
 #include <vector>
+#include <algorithm>
+#include <random>
+#include <iterator>
 
- using namespace std;
-
-int Partition(vector<int> &arr, int low, int high){
-    int pivot = arr[high];
-    int i = low - 1, temp;
-
-    for(int j = low; j < high; j++){
-        if(arr[j] <= pivot){
-            i += 1;
-            temp = arr[i];
-            arr[i] = arr[j];
-            arr[j] = temp;
-        }
-    }
-
-    temp = arr[i+1];
-    arr[i+1] = arr[high];
-    arr[high] = temp;
-
-    return (i+1);
+template<typename T>
+void swap(T& a, T& b)
+{
+    int temp = a;
+    a = b;
+    b = temp;
 }
 
-void QuickSort(vector<int> &arr, int low, int high){
-    if(low < high){
+template<typename T>
+int partition (std::vector<T> &arr, int low, int high) 
+{ 
+    int pivot = arr[high];    // pivot 
+    int i = (low - 1);  // Index of smaller element 
+  
+    for (int j = low; j <= high- 1; j++) 
+    { 
+        // If current element is smaller than or 
+        // equal to pivot 
+        if (arr[j] <= pivot) 
+        { 
+            i++;    // increment index of smaller element 
+            swap(arr[i], arr[j]); 
+        } 
+    } 
+    swap(arr[i + 1], arr[high]); 
+    return (i + 1); 
+} 
+  
+template<typename T>
+void quick_sort(std::vector<T> &arr, int low, int high) 
+{ 
+    if (low < high) 
+    { 
+        /* pi is partitioning index, arr[p] is now 
+           at right place */
+        int pi = partition(arr, low, high); 
+  
+        // Separately sort elements before 
+        // partition and after partition 
+        quick_sort(arr, low, pi - 1); 
+        quick_sort(arr, pi + 1, high); 
+    } 
+} 
 
-        int mid = Partition(arr, low, high);
-
-        QuickSort(arr, low, mid - 1);
-        QuickSort(arr, mid + 1, high);
-    }
+template<typename T>
+void print_vector(const std::vector<T> &vec)
+{
+    std::copy(vec.begin(), vec.end(), std::ostream_iterator<T>(std::cout, ", "));
 }
 
-int main(){
-    vector<int> arr;
-    int inpt;
+int main()
+{
+    const int data_size = 20;
+    std::random_device device;
+    std::default_random_engine engine(device());
+    std::uniform_int_distribution<int> distribution(1, 1000);
 
-    cout << "Unsorted Input -- ";
-    while(cin.peek() != '\n'){
-        cin >> inpt;
-        arr.push_back(inpt);
-    }
+    std::vector<int> test(data_size);
+    std::generate(test.begin(), test.end(), [&]()
+    {
+        return distribution(engine);
+    });
 
-    QuickSort(arr, 0, arr.size()-1);
+    std::cout <<"Data before: ";
+    print_vector(test);
+    std::cout << std::endl;
+    quick_sort(test, 0, test.size() - 1);
 
-    cout << "\nSorted Output -- ";
-
-    for(auto i: arr){ cout << i << " "; }  // if your compiler supports C++11 or above
-    
-    //for(int i = 0; i < arr.size(); i++){ cout << arr[i] << " "; } //if your compiler doesn't support C++11 or above
+    std::cout << "Data after: ";
+    print_vector(test);
+    std::cout << std::endl;
     return 0;
 }
