f8252983796d0d78f25053bd0af4b2f0f7eecdbe
Souma Kanti Ghosh <40127554+SubCoder1@users.noreply.github.com>
2018-10-19 16:22:37 +0530

    euclidean gcd implementation in C++

--- euclidean_gcd/cpp/euclidean_gcd.cpp
+++ euclidean_gcd/cpp/euclidean_gcd.cpp
@@ -1,2 +1,15 @@
 // add your code here
+#include <bits/stdc++.h>
+ using namespace std;
 
+int euclidean_gcd(int num1, int num2){
+  if(num1 == 0){ return num2; }
+  return euclidean_gcd(num2 % num1, num1) }
+
+int main(){
+  int a = 10;
+  int b = 15;
+  cout << "gcd of " << a << " & " << b << " -> " << euclidean_gcd(a,b) << endl;
+  return 0;
+}
+  
