5f517eacf1c97b7a1c482b08faa66e5c07e7c0f8
Souma Kanti Ghosh <40127554+SubCoder1@users.noreply.github.com>
2018-10-19 16:18:30 +0530

    Update euclidean_gcd.py

--- euclidean_gcd/python/euclidean_gcd.py
+++ euclidean_gcd/python/euclidean_gcd.py
@@ -1,1 +1,14 @@
-# add your code here
+# Function to return gcd of a and b 
+
+def gcd(num_1, num_2):  
+    if num_1 == 0 : 
+        return num_2  
+      
+    return gcd(num_2 % num_1, num_1) 
+  
+print("Enter the 1st number -- , end='')
+num_1 = int(input())
+print("Enter the 2nd number -- , end='')
+num_2 = int(input())
+
+print("gcd of {} and {} -> ".format(num_1,num_2), gcd(num_1, num_2) ,end='')
