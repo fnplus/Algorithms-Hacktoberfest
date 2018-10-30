2a08ef2d081a69c4c90233faeeb013931f4c03f6
Jeremias Moreira Gomes <j3r3miasmg@gmail.com>
2018-10-30 01:55:25 -0300

    euclidean_gcd.cpp Add GCD Algorithm
    This adds GCD Algorithm which return the Greatest Commom Divisor between
    two numbers.
    
    GCD in C++.

--- euclidean_gcd/cpp/euclidean_gcd.cpp
+++ euclidean_gcd/cpp/euclidean_gcd.cpp
@@ -1,2 +1,17 @@
-// add your code here
+#include <iostream>
 
+int gcd(int a, int b)
+{
+    return b == 0 ? a : gcd(b, a % b);
+}
+
+using namespace std;
+int main()
+{
+    int a, b;
+    cout << "Inform the vales of a and b: ";
+    cin >> a;
+    cin >> b;
+    cout << "GCD(" << a << ", " << b << "): " << gcd(a, b) << endl;
+    return 0;
+}
