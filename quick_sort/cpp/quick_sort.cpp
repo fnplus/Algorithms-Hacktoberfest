aa2f5cc6aeafaa0a6ad8f767edd9cc7ee02de767
Prateek <43875858+prateekyo@users.noreply.github.com>
2019-10-02 16:43:51 +0530

    I think this is a better code for quick sort

--- quick_sort/cpp/quick_sort.cpp
+++ quick_sort/cpp/quick_sort.cpp
@@ -1,79 +1,73 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
-#include <random>
-#include <iterator>
+/* C implementation QuickSort */
+#include<stdio.h> 
 
-template<typename T>
-void swap(T& a, T& b)
-{
-    int temp = a;
-    a = b;
-    b = temp;
-}
-
-template<typename T>
-int partition (std::vector<T> &arr, int low, int high) 
+// A utility function to swap two elements 
+void swap(int* a, int* b) 
 { 
-    int pivot = arr[high];    // pivot 
-    int i = (low - 1);  // Index of smaller element 
-  
-    for (int j = low; j <= high- 1; j++) 
-    { 
-        // If current element is smaller than or 
-        // equal to pivot 
-        if (arr[j] <= pivot) 
-        { 
-            i++;    // increment index of smaller element 
-            swap(arr[i], arr[j]); 
-        } 
-    } 
-    swap(arr[i + 1], arr[high]); 
-    return (i + 1); 
-} 
-  
-template<typename T>
-void quick_sort(std::vector<T> &arr, int low, int high) 
-{ 
-    if (low < high) 
-    { 
-        /* pi is partitioning index, arr[p] is now 
-           at right place */
-        int pi = partition(arr, low, high); 
-  
-        // Separately sort elements before 
-        // partition and after partition 
-        quick_sort(arr, low, pi - 1); 
-        quick_sort(arr, pi + 1, high); 
-    } 
+	int t = *a; 
+	*a = *b; 
+	*b = t; 
 } 
 
-template<typename T>
-void print_vector(const std::vector<T> &vec)
-{
-    std::copy(vec.begin(), vec.end(), std::ostream_iterator<T>(std::cout, ", "));
-}
+/* This function takes last element as pivot, places 
+the pivot element at its correct position in sorted 
+	array, and places all smaller (smaller than pivot) 
+to left of pivot and all greater elements to right 
+of pivot */
+int partition (int arr[], int low, int high) 
+{ 
+	int pivot = arr[high]; // pivot 
+	int i = (low - 1); // Index of smaller element 
 
-int main()
-{
-    const int data_size = 20;
-    std::random_device device;
-    std::default_random_engine engine(device());
-    std::uniform_int_distribution<int> distribution(1, 1000);
+	for (int j = low; j <= high- 1; j++) 
+	{ 
+		// If current element is smaller than or 
+		// equal to pivot 
+		if (arr[j] <= pivot) 
+		{ 
+			i++; // increment index of smaller element 
+			swap(&arr[i], &arr[j]); 
+		} 
+	} 
+	swap(&arr[i + 1], &arr[high]); 
+	return (i + 1); 
+} 
 
-    std::vector<int> test(data_size);
-    std::generate(test.begin(), test.end(), [&]()
-    {
-        return distribution(engine);
-    });
+/* The main function that implements QuickSort 
+arr[] --> Array to be sorted, 
+low --> Starting index, 
+high --> Ending index */
+void quickSort(int arr[], int low, int high) 
+{ 
+	if (low < high) 
+	{ 
+		/* pi is partitioning index, arr[p] is now 
+		at right place */
+		int pi = partition(arr, low, high); 
 
-    std::cout <<"Data before: ";
-    print_vector(test);
-    std::cout << std::endl;
-    quick_sort(test, 0, test.size() - 1);
+		// Separately sort elements before 
+		// partition and after partition 
+		quickSort(arr, low, pi - 1); 
+		quickSort(arr, pi + 1, high); 
+	} 
+} 
 
-    std::cout << "Data after: ";
-    print_vector(test);
-    std::cout << std::endl;
-    return 0;
-}
+/* Function to print an array */
+void printArray(int arr[], int size) 
+{ 
+	int i; 
+	for (i=0; i < size; i++) 
+		printf("%d ", arr[i]); 
+	printf("n"); 
+} 
+
+// Driver program to test above functions 
+int main() 
+{ 
+	int arr[] = {10, 7, 8, 9, 1, 5}; 
+	int n = sizeof(arr)/sizeof(arr[0]); 
+	quickSort(arr, 0, n-1); 
+	printf("Sorted array: n"); 
+	printArray(arr, n); 
+	return 0; 
+} 
