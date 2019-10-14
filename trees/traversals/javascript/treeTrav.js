3ab7443cc6e2de1e0fa927904ce5fe01e4481bc4
deyRupak <rupakdey9999@gmail.com>
2019-10-14 19:50:00 +0530

    creating a tree

--- trees/traversals/javascript/treeTrav.js
+++ trees/traversals/javascript/treeTrav.js
@@ -5,3 +5,16 @@ function Node(data) {
     this.left = null;
     this.right = null;
 }
+
+// Creating Nodes
+var root = new Node(1);
+var n1 = new Node(2);
+var n2 = new Node(3);
+var n3 = new Node(4);
+var n4 = new Node(5);
+
+// Setting Children
+root.left = n1;
+root.right = n2;
+n1.left = n3;
+n1.right = n4;
