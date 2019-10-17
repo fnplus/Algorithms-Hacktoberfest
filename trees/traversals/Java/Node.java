67894d6c51e0464a72901d5a9c78d37ea5e12ea9
Your Name <you@example.com>
2019-10-17 20:31:57 +0530

    pre order traversal in java

---
+++ trees/traversals/Java/Node.java
@@ -0,0 +1,47 @@
+
+class Node 
+{ 
+	int key; 
+	Node left, right; 
+
+	public Node(int item) 
+	{ 
+		key = item; 
+		left = right = null; 
+	} 
+} 
+
+class BTree 
+{ 
+	Node root; 
+
+	BTree() 
+	{ 
+		root = null; 
+	} 
+
+	void printPreorder(Node node) 
+	{ 
+		if (node == null) 
+			return; 
+
+		System.out.print(node.key + " "); 
+		printPreorder(node.left); 
+     	printPreorder(node.right); 
+	} 
+
+	void printPreorder() {	 printPreorder(root); } 
+	public static void main(String[] args) 
+	{ 
+		BTree tree = new BTree(); 
+		tree.root = new Node(1); 
+		tree.root.left = new Node(2); 
+		tree.root.right = new Node(3); 
+		tree.root.left.left = new Node(4); 
+		tree.root.left.right = new Node(5); 
+
+		System.out.println("Preorder traversal of binary tree is "); 
+		tree.printPreorder(); 
+
+	} 
+} 
