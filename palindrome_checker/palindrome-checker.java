ff1e47043c1ba7ce53392eb9135fcdc908fecabe
darshita-kumar <kumardarshita18@gmail.com>
2019-10-19 16:34:54 +0530

    palindrome-checker.java Added Palindrome checker in java. This accepts a word as an input and checks if the string is palindrome or not

---
+++ palindrome_checker/palindrome-checker.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+	    System.out.println("Enter the string you want to check: ");
+	    String input = sc.next();
+	    
+	    StringBuffer str = new StringBuffer(input);
+	    str.reverse();
+	    System.out.println(str);
+	    
+	    if(input.equals(str.toString()))
+	        System.out.println("The word is palindrome.");
+	    else
+	        System.out.println("The string is not palindrome.");
+    }
+}
