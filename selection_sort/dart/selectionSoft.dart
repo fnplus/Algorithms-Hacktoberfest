4a0c68487420ea5a35583e352d164610b8118591
coder7eeN <phamnguyenanhhuy@gmail.com>
2019-10-19 16:10:45 +0700

    selectionSort.dart: add selection sort algorithm

---
+++ selection_sort/dart/selectionSoft.dart
@@ -0,0 +1,23 @@
+main() {
+  selectionSort([173, 76, 90, 1, 0]);
+}
+
+void selectionSort(List list) {
+  if (list == null || list.length == 0) return;
+  int n = list.length;
+  int i, steps;
+  for (steps = 0; steps < n; steps++) {
+    for (i = steps + 1; i < n; i++) {
+      if(list[steps] > list[i]) {
+        swap(list, steps, i);
+      }
+    }
+  }
+  print(list);
+}
+
+void swap(List list, int steps, int i) {
+  int temp = list[steps];
+  list[steps] = list[i];
+  list[i] = temp;
+}
