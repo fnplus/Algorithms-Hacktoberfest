f89c44313eaf618039c3e716a4003cf29f799012
GitanshKapoor <72307552+GitanshKapoor@users.noreply.github.com>
2020-10-03 19:33:21 +0530

    Update bubble_sort.cpp

--- bubble_sort/cpp/bubble_sort.cpp
+++ bubble_sort/cpp/bubble_sort.cpp
@@ -1,25 +1,18 @@
 #include <iostream>
 
-void swap(int& x, int & y){
-	// Swap without using extra variable
-	x = x + y;
-	y = x - y;
-	x = x - y;
-}
 
-void bubblesort(int a_size, int *a) {
-	bool swapped = true; //variable to let us know if we have made a swap
-	int j = 0;
-	while(swapped) {
-		swapped = false;
-		j++;
-		for(int i=0; i<a_size-j; i++) { //check each index and compare its element to the i + 1 element
-			if(a[i] > a[i + 1]){
-				swap(a[i], a[i + 1]);
-				swapped = true;
-			} //if out of order, swap
-		}
-	} //loop breaks when a full pass through array without a single swap, meaning the array is sorted
+
+void bubblesort (int n, int a[]) {
+    int i, j, t;
+    for(i = 0; i < n-1; ++i) {
+        for(j = 0; j < n-i-1; ++j) {
+            if(a[j+1] < a[j]) {
+                t = a[j];
+                a[j] = a[j+1];
+                a[j+1] = t;
+            }
+        }
+    }
 }
 
 void print_array(int a_size, int *a) {
