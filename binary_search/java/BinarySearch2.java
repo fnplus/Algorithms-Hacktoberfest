8b87ad360a26e7c34a9ea8a5a6f8e19b14d129db
swapnita-pandey <81066748+swapnita-pandey@users.noreply.github.com>
2021-10-05 00:42:33 +0530

    Update BinarySearch2.java (#417)

--- binary_search/java/BinarySearch2.java
+++ binary_search/java/BinarySearch2.java
@@ -2,23 +2,22 @@
 class BinarySearch { 
     // Returns index of x if it is present in arr[], 
     // else return -1 
-    int binarySearch(int arr[], int x) 
+    int binarySearch(int arr[], int l, int r, int x) 
     { 
-        int l = 0, r = arr.length - 1; 
         while (l <= r) { 
-            int m = l + (r - l) / 2; 
+            int mid = l + (r - l) / 2; 
   
             // Check if x is present at mid 
-            if (arr[m] == x) 
-                return m; 
-  
-            // If x greater, ignore left half 
-            if (arr[m] < x) 
-                l = m + 1; 
+            if (arr[mid] == x) 
+                return mid; 
   
             // If x is smaller, ignore right half 
+            if (arr[mid] > x) 
+                return binarySearch(arr, l, mid-1, x);
+  
+            // If x greater, ignore left half
             else
-                r = m - 1; 
+                return binarySearch(arr, mid+1, r, x);
         } 
   
         // if we reach here, then element was 
@@ -32,8 +31,8 @@ class BinarySearch {
         BinarySearch ob = new BinarySearch(); 
         int arr[] = { 2, 3, 4, 10, 40 }; 
         int n = arr.length; 
-        int x = 10; 
-        int result = ob.binarySearch(arr, x); 
+        int x = 10; //this is the number to be searched
+        int result = ob.binarySearch(arr, 0, n-1, x); 
         if (result == -1) 
             System.out.println("Element not present"); 
         else
